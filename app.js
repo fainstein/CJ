b4aa7067d5a936f76b93219aeea1acf555818279
fainstein <72775588+fainstein@users.noreply.github.com>
2021-03-26 19:58:43 -0300

    Correcting app listener

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.get("/", function(req,res){
   res.render("index");
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server running on port 3000");
 });
