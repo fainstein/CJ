0d27f26f4689baf60721f771c519f3aa4ef8d270
fainstein <72775588+fainstein@users.noreply.github.com>
2021-08-31 16:45:19 -0300

    no log in version

--- public/index.js
+++ public/index.js
@@ -33,8 +33,31 @@ const swiper = new Swiper('.swiper-container', {
   },
 });
 
-// $('.dropdown-trigger').dropdown();
+// Form char counter
+$(document).ready(function() {
+  $('input#input_text, textarea#message').characterCounter();
+});
 
-$(".dropdown-menu li a").click(function(){
-  $(".btn:first-child").html($(this).text()+' <span class="caret"></span>');
+// Form select reason
+$(document).ready(function(){
+  $('select').formSelect();
+});
+
+
+// New Recipe
+
+$(document).ready(function(){
+  $('input.autocomplete').autocomplete({
+    data: {
+      "Cucharada": null,
+      "Cucharadita": null,
+      "Gramos": null,
+      "Miligramos": null,
+      "Kilogramos": null,
+      "Taza": null,
+      "Litro": null,
+      "Mililitro": null,
+      "Centímetro cúbico": null,
+    },
+  });
 });
